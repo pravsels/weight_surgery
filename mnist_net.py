e8b37a64bcac12b2b566eb7bf7d30aedf3e6aa21
Praveen Selvaraj <praveen.beatle@gmail.com>
2023-04-07 21:03:09 +0100

    non-square weights matrix check

--- mnist_net.py
+++ mnist_net.py
@@ -14,8 +14,8 @@ class Net(nn.Module):
 
         self.fc1 = nn.Linear(64 * 12 * 12, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 3)
-        self.fc4 = nn.Linear(3, 3)
+        self.fc3 = nn.Linear(64, 16)
+        self.fc4 = nn.Linear(16, 3)
         self.fc5 = nn.Linear(3, 10)
 
 
